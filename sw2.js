9ac7be386b8cb563e80929b48b3aee11b7b8367b
khulas18 <khulas_18@yahoo.com>
2014-08-09 14:40:05 +0800

    minimize the line

--- sw2.js
+++ sw2.js
@@ -1,13 +1,8 @@
-var coe = function(){
-	for (var i=1; i<101 ; i++){
-		if(i%3==0 && i %5==0){
-			console.log("ComputerEngineering");
-		}else if(i%5==0){
-			console.log("Engineering")
-		}else if(i%3==0){
-			console.log("Computer")
-		}else{
-			console.log(i);
+	var coe = function(){
+		for (var i=1; i<101 ; i++){
+			var str = "";
+			str = (i%3==0)?"Computer":"";
+			str = (i%5==0)?str+"Engineering":str;
+			console.log(str || i);
 		}
 	}
-}
